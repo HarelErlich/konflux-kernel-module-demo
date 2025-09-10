fa5d89e6c0f6b83ef40a88bda41a66f990c53023
Harel Erlich <herlich@herlich-thinkpadp1gen7.raanaii.csb>
2025-09-10 15:32:21 +0300

    Update kernel module message to v2
    Test build after fixing Quay authentication

--- hello-world.c
+++ hello-world.c
@@ -7,7 +7,7 @@ MODULE_AUTHOR("Harel Erlich");
 MODULE_DESCRIPTION("A simple Hello World kernel module.");
 
 static int __init hello_init(void) {
-    printk(KERN_INFO "Hello, World! from the kernel module.\n");
+    printk(KERN_INFO "Hello, World! from the kernel module v2.\n");
     return 0;
 }
 
