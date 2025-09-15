7a963d630dc0174bdccbfc3b0f21d498cb2df9d4
Harel Erlich <herlich@herlich-thinkpadp1gen7.raanaii.csb>
2025-09-15 13:08:05 +0300

    Changeing only for debug

--- hello-world.c
+++ hello-world.c
@@ -7,12 +7,12 @@ MODULE_AUTHOR("Harel Erlich");
 MODULE_DESCRIPTION("A simple Hello World kernel module.");
 
 static int __init hello_init(void) {
-    printk(KERN_INFO "Hello, World! from the kernel module v3.\n");
+    printk(KERN_INFO "HELLO, WORLD! from the kernel module v3.\n");
     return 0;
 }
 
 static void __exit hello_exit(void) {
-    printk(KERN_INFO "Goodbye, World! from the kernel module.\n");
+    printk(KERN_INFO "GOODBYE, WORLDs! from the kernel module.\n");
 }
 
 module_init(hello_init);
